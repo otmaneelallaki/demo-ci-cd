1d70918e59fa7ec94da4b6a4665f436c00821ada
otmaneelllaki <otmane.allaki1@gmail.com>
2024-03-22 23:38:44 +0100

    test

--- python_lambda_template/index.py
+++ python_lambda_template/index.py
@@ -2,5 +2,5 @@ def handler(event, context):
     return {
         "statusCode": 200,
         "headers": {"Content-Type": "application/txt"},
-        "body": "Template executed properly !",
-    }
+        "body": "Template executed properly ! 2222222",
+    } 
