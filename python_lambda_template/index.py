d9d8593694bec599837d4682985d2dd4cd4b7e19
otmaneelllaki <otmane.allaki1@gmail.com>
2024-03-27 11:50:41 +0100

    test data

--- python_lambda_template/index.py
+++ python_lambda_template/index.py
@@ -1,6 +1,12 @@
+import pandas as pd
 def handler(event, context):
+
+    dic = {"name":["otmane", "imane"], 
+           "prenom":["chouli", "tribak"]}
+    data = pd.DataFrame(dic)
+
     return {
         "statusCode": 200,
         "headers": {"Content-Type": "application/txt"},
-        "body": "Template executed properly !",
+        "body": data,
     }
